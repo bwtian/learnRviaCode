be78d50d6aa9b41f11d3e11449c73624cbedba83
Bingwei Tian <bwtian@gmail.com>
2014-09-21 12:48:58 +0900

    + ‘R/catR2R.R’

---
+++ R/catR2R.R
@@ -0,0 +1,20 @@
+catR2R  <- function(dir, type = "R"){
+        dir  <- getwd()
+        # TODO , type =  c("R","Rnw","Rmd","org")
+        #files  <- list.files(path = dir, patter = ".[rR]$", recursive = FALSE,)
+        files  <- list.files(path = dir, recursive = T)
+        now <- format(Sys.time(), "_%y%m%d_%H%M%S")
+        prefix  <- "00Merged"
+        if (type == "R"){
+                fileName  <- paste( prefix, now, ".R", sep = "")
+        }
+
+        for (i in files) {
+                title  <- paste("#'", basename(i))
+                content <- readLines(i)
+                if (type == "R"){
+                        write.table(c(title,content), fileName, append = TRUE, quote = FALSE,col.names = FALSE,row.names = FALSE)
+                }
+
+        }
+}

917b38bef0e330eab8b7036e223b6a915fdfae33
t3500 <bwtian@gmail.com>
2014-09-22 16:26:05 +0900

    - ‘R/.#catR2org.R’ / ‘R/catR2org.R’

--- R/catR2org.R
+++ R/catR2org.R
@@ -9,32 +9,31 @@
 #' @author Bingwei Tian <bwtian@gmail.com>
 #' @example catR2org("landsat")
 catR2org  <- function(pkgRepo, pkgWD){
-                pkgWD  <- getwd()
-                pkgDir <- file.path(pkgWD,pkgRepo)
-                des1   <- readLines(file.path(pkgDir,"DESCRIPTION"))
-                des2   <- gsub(pattern ="(^[^:]+):(.+$)",
-                                  replacement ="\\1%\\2",
-                                  x           =des1)
+                pkgWD     <- getwd()
+                pkgDir    <- file.path(pkgWD,pkgRepo)
+                des1      <- readLines(file.path(pkgDir,"DESCRIPTION"))
+                des2      <- gsub(pattern  = "(^[^:]+):(.+$)",
+                                  replacement  = "\\1%\\2", x = des1)
                 des3      <- paste0("+ ", des1)
                 des       <- as.data.frame(do.call(rbind,strsplit(des2, split = "%")))
-                pkgVer    <-des[des[,1]                                    == "Version",][,2]
-                pkgVer    <-gsub("(^ *)|( *$)", "", pkgVer)
-                pkgName   <- des[des[,1]                                  == "Package",][,2]
-                pkgName   <-gsub("(^ *)|( *$)", "", pkgName)
-                pkgDate   <- des[des[,1]                                  == "Date",][,2]
-                pkgDate   <-gsub("(^ *)|( *$)", "", pkgDate)
-                pkgAuthor <- des[des[,1]                                == "Author",][,2]
-                pkgAuthor <-gsub("(^ *)|( *$)", "", pkgAuthor)
-                pkgAuthor <-gsub("\\s", "-", pkgAuthor)
+                pkgVer    <- des[des[,1] == "Version",][,2]
+                pkgVer    <- gsub("(^ *)|( *$)", "", pkgVer)
+                pkgName   <- des[des[,1] == "Package",][,2]
+                pkgName   <- gsub("(^ *)|( *$)", "", pkgName)
+                pkgDate   <- des[des[,1] == "Date",][,2]
+                pkgDate   <- gsub("(^ *)|( *$)", "", pkgDate)
+                pkgAuthor <- des[des[,1] == "Author",][,2]
+                pkgAuthor <- gsub("(^ *)|( *$)", "", pkgAuthor)
+                pkgAuthor <- gsub("\\s", "-", pkgAuthor)
                 orgName   <- paste0(pkgName, "_", pkgVer, "_(", pkgDate, "_by_",pkgAuthor,").org")
-                write.table(des3, orgName, sep                           = ":",  append = T,quote = F, row.names = F, col.names = F)
-                rPath     <- file.path(pkgDir, "/R")
-                rFiles    <- list.files(path                               = rPath, pattern = "[rR]$")
+                write.table(des3, orgName, sep = ":",  append = T,quote = F, row.names = F, col.names = F)
+                rPath  <- file.path(pkgDir, "/R")
+                rFiles <- list.files(path   = rPath, pattern = "[rR]$")
                 for (i in c(rFiles)){
-                        heading  <- paste0("* ", i)
-                        header  <- "#+BEGIN_SRC R "
-                        rfile  <- read.table(file = file.path(rPath,i), sep = "\n", comment.char = "")
-                        ender  <- "#+END_SRC"
+                        heading <- paste0("* ", i)
+                        header  <- " #+BEGIN_SRC R "
+                        rfile   <- read.table(file = file.path(rPath,i), sep = "\n", comment.char = "")
+                        ender   <- "  #+END_SRC"
                         write.table(rbind(heading, header,rfile, ender), orgName, sep = "\n", 
                                     append = T,quote = F, row.names = F, col.names = F)
                 }

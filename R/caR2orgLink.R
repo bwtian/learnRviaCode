9749d11efa9872330a9184fe0366e72c4f327b5a
t3500 <bwtian@gmail.com>
2014-10-07 15:23:38 +0900

    / ‘R/caR2orgLink.R’

--- R/caR2orgLink.R
+++ R/caR2orgLink.R
@@ -1,4 +1,8 @@
-catR2orgLink  <- function(dir = getwd()){
+#' Cat R files in the folder to Org file links
+#' @export
+#' @usage  catR2orgLink()
+catR2orgLink  <- function(dir = getwd(), full.path = FALSE) {
+        ## todo path = absolute or not
 rFiles <- list.files(path = dir, pattern = "[rR]$", full.name = T)
 for (i in c(rFiles)) {
         header1  <- paste0("** ","file:", tools::file_path_as_absolute(i))
@@ -7,5 +11,4 @@ for (i in c(rFiles)) {
                     append = T,quote = F, row.names = F, col.names = F)
 }
 }
-setwd("~/SparkleShare/geothermaR/R/")
-catR2orgLink()
+

5d17df0e1bb87ac141a66d66e5f020dc96f775bd
t3500 <bwtian@gmail.com>
2014-10-07 15:25:45 +0900

    / ‘R/caR2orgLink.R’

--- R/caR2orgLink.R
+++ R/caR2orgLink.R
@@ -5,10 +5,12 @@ catR2orgLink  <- function(dir = getwd(), full.path = FALSE) {
         ## todo path = absolute or not
 rFiles <- list.files(path = dir, pattern = "[rR]$", full.name = T)
 for (i in c(rFiles)) {
-        header1  <- paste0("** ","file:", tools::file_path_as_absolute(i))
+        name  <- basename(i)
+        header1  <- paste0("** ",name ," file:", tools::file_path_as_absolute(i))
         header  <- gsub("\\/home\\/.+\\/SparkleShare","~\\/SparkleShare", header1)
         write.table(header,
                     append = T,quote = F, row.names = F, col.names = F)
 }
 }
 
+catR2orgLink()

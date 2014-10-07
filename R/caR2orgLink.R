9c2b042e57fd4ce4fc2fa4bde44a915fc5970582
t3500 <bwtian@gmail.com>
2014-10-07 15:18:15 +0900

    / ‘R/caR2orgLink.R’

--- R/caR2orgLink.R
+++ R/caR2orgLink.R
@@ -2,7 +2,7 @@ setwd("~/SparkleShare/geothermaR/R/")
 rFiles <- list.files(path = getwd(), pattern = "[rR]$", full.name = T)
 for (i in c(rFiles)) {
         header1  <- paste0("** ","file:", tools::file_path_as_absolute(i))
-        header  <- gsub("\\/home\\/.+\\/S","~\\/S", header1)
+        header  <- gsub("\\/home\\/.+\\/SparkleShare","~\\/SparkleShare", header1)
         write.table(header,
                     append = T,quote = F, row.names = F, col.names = F)
 }

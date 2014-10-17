bf0958e800ffd2be8a02dd29262102ccce263fdf
t3500 <bwtian@gmail.com>
2014-10-17 17:51:29 +0900

    / ‘R/catRpkg2org.R’

--- R/catRpkg2org.R
+++ R/catRpkg2org.R
@@ -37,7 +37,7 @@ catRpkg2org  <- function(pkgName, pkgDir, orgDir){
                         write.table(rbind(heading, header,rfile, ender), orgName, sep = "\n",
                                     append = T,quote = F, row.names = F, col.names = F)
                 }
-                message("Wrap R code files to Emacs Org-mode babel file Finished")
+#                message("Wrap R code files to Emacs Org-mode babel file Finished")
 #                 for (i in list.files(pattern = "*.org$")) {
 #                 file.copy(from = i,  to = orgDir )
 #                 file.remove(i)
